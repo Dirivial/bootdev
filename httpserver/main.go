ca2d2f970c61b5cc2051870b45c496a1cb749d0c
Dirivial <alexander.kadeby@gmail.com>
2026-04-15 17:34:55 +0200

    CH1L4

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+	fmt.Print("Starting server")
+
+	serveMux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+
+	server.ListenAndServe()
 }
