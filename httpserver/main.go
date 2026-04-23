69c1edbbf2dcf2cba3299e85334f82b6817bcb35
Dirivial <alexander.kadeby@gmail.com>
2026-04-23 08:02:19 +0200

    CH2L2

--- httpserver/main.go
+++ httpserver/main.go
@@ -4,15 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (c *apiConfig) IncFileServerHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (c *apiConfig) HandleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d\n", c.fileServerHits.Load())
+}
+
+func (c *apiConfig) HandleReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	c.fileServerHits.Store(0)
+}
 
 func main() {
 
 	serveMux := http.NewServeMux()
+	c := &apiConfig{}
 
 	fileServer := http.FileServer(http.Dir("./html"))
+	fileServer = http.StripPrefix("/app", fileServer)
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	serveMux.Handle("/app/", c.IncFileServerHits(fileServer))
 
 	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -20,6 +46,9 @@ func main() {
 
 		w.Write([]byte("OK"))
 	})
+
+	serveMux.HandleFunc("/metrics", c.HandleMetrics)
+	serveMux.HandleFunc("/reset", c.HandleReset)
 
 	server := http.Server{
 		Addr:    ":8080",
