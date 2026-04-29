5b8345b335ff774bec427098c58fc3a417b3e631
Dirivial <alexander.kadeby@gmail.com>
2026-04-29 19:50:17 +0200

    CH6L7: Dockerizing Python

--- docker/python/Dockerfile.py
+++ docker/python/Dockerfile.py
@@ -1,4 +1,7 @@
 FROM debian:stable-slim
 COPY main.py main.py
 COPY books/ books/
-CMD ["python", "main.py"]
+
+RUN apt-get update && apt-get install -y python3 python3-pip
+
+CMD ["python3", "main.py"]
