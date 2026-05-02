cc41e4705f065f25d4b2cd8fb5d8db62d258ac45
Dirivial <alexander.kadeby@gmail.com>
2026-05-02 12:07:06 +0200

    CH4L4: JSON

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -30,6 +31,67 @@ func (c *apiConfig) HandleReset(w http.ResponseWriter, r *http.Request) {
 	c.fileServerHits.Store(0)
 }
 
+func writeError(w http.ResponseWriter, status int, message string) {
+	type ErrResponse struct {
+		Error string `json:"error"`
+	}
+
+	errResponse := ErrResponse{Error: message}
+	dat, err := json.Marshal(errResponse)
+	if err != nil {
+		log.Printf("Error marshalling JSON response: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dat)
+}
+
+func (c *apiConfig) HandleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type Chirp struct {
+		Body string `json:"body"`
+	}
+
+	if r.Method != "POST" {
+		writeError(w, http.StatusMethodNotAllowed, "Invalid method")
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		writeError(w, http.StatusUnsupportedMediaType, "Invalid Content-Type")
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chirp := Chirp{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "Invalid JSON")
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		writeError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	type ValidResponse struct {
+		Valid bool `json:"valid"`
+	}
+	validResponse := ValidResponse{true}
+	dat, err := json.Marshal(validResponse)
+	if err != nil {
+		log.Printf("Error marshalling JSON response: %s", err)
+		writeError(w, http.StatusInternalServerError, "Error creating JSON response")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dat)
+}
+
 func main() {
 
 	serveMux := http.NewServeMux()
@@ -46,6 +108,8 @@ func main() {
 
 		w.Write([]byte("OK"))
 	})
+
+	serveMux.HandleFunc("POST /api/validate_chirp", c.HandleValidateChirp)
 
 	serveMux.HandleFunc("GET /api/metrics", c.HandleMetrics)
 	serveMux.HandleFunc("POST /api/reset", c.HandleReset)
